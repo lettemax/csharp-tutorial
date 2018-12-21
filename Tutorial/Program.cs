b7003e242787aa95c37e6bb196a7406741c94a39
Max Lettenberger <maxlettenberger@gmail.com>
2018-12-20 22:21:18 -0600

    Initial commit

---
+++ Tutorial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Tutorial
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //BuildWebHost(args).Run();
+
+            // Write to console
+            Console.WriteLine("Welcome to the C# Station Tutorial!");
+
+            // keep screen from going away
+            // when run from VS.NET
+            Console.ReadLine();
+        }
+
+
+
+        //public static IWebHost BuildWebHost(string[] args) =>
+            //WebHost.CreateDefaultBuilder(args)
+                //.UseStartup<Startup>()
+                //.Build();
+    }
+}
